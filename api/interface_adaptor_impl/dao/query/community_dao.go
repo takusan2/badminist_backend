9520af35da93dcb047be5af32edd152dba55dbeb
hapitaku <106806920+takuya-okada-01@users.noreply.github.com>
2023-09-14 22:51:08 +0900

    logger

--- api/interface_adaptor_impl/dao/query/community_dao.go
+++ api/interface_adaptor_impl/dao/query/community_dao.go
@@ -1,6 +1,7 @@
 package query_dao
 
 import (
+	"github.com/sirupsen/logrus"
 	"github.com/takuya-okada-01/badminist/api/domain/community"
 	"github.com/takuya-okada-01/badminist/api/domain/community/member"
 	"github.com/takuya-okada-01/badminist/api/domain/community/player"
@@ -22,6 +23,9 @@ func (*CommunityDaoImpl) FindCommunitiesByUserId(
 ) ([]entity.Community, error) {
 	var members []entity.Member
 	if err := db.Where("user_id = ?", userId.Value()).Find(&members).Error; err != nil {
+		logrus.WithFields(logrus.Fields{
+			"function": "FindCommunitiesByUserId",
+		}).Error(err)
 		return nil, err
 	}
 	var communityIds []string
@@ -30,8 +34,15 @@ func (*CommunityDaoImpl) FindCommunitiesByUserId(
 	}
 	var communities []entity.Community
 	if err := db.Where("id IN ?", communityIds).Find(&communities).Error; err != nil {
+		logrus.WithFields(logrus.Fields{
+			"function": "FindCommunitiesByUserId",
+		}).Error(err)
 		return nil, err
 	}
+
+	logrus.WithFields(logrus.Fields{
+		"function": "FindCommunitiesByUserId",
+	}).Info(communities)
 	return communities, nil
 }
 
@@ -41,8 +52,14 @@ func (*CommunityDaoImpl) FindCommunityById(
 ) (entity.Community, error) {
 	var community entity.Community
 	if err := db.Where("id = ?", communityId.Value()).First(&community).Error; err != nil {
+		logrus.WithFields(logrus.Fields{
+			"function": "FindCommunityById",
+		}).Error(err)
 		return entity.Community{}, err
 	}
+	logrus.WithFields(logrus.Fields{
+		"function": "FindCommunityById",
+	}).Info(community)
 	return community, nil
 }
 
@@ -51,9 +68,18 @@ func (*CommunityDaoImpl) FindMemberById(
 	memberId member.MemberId,
 ) (entity.Member, error) {
 	var member entity.Member
-	if err := db.First(&member, memberId.Value()).Error; err != nil {
+	if err := db.
+		Preload("User").
+		First(&member, memberId.Value()).
+		Error; err != nil {
+		logrus.WithFields(logrus.Fields{
+			"function": "FindMemberById",
+		}).Error(err)
 		return entity.Member{}, err
 	}
+	logrus.WithFields(logrus.Fields{
+		"function": "FindMemberById",
+	}).Info(member)
 	return member, nil
 }
 
@@ -62,9 +88,19 @@ func (*CommunityDaoImpl) FindMembersByCommunityId(
 	communityId community.CommunityId,
 ) ([]entity.Member, error) {
 	var members []entity.Member
-	if err := db.Where("community_id = ?", communityId.Value()).Find(&members).Error; err != nil {
+	if err := db.
+		Preload("User").
+		Where("community_id = ?", communityId.Value()).
+		Find(&members).
+		Error; err != nil {
+		logrus.WithFields(logrus.Fields{
+			"function": "FindMembersByCommunityId",
+		}).Error(err)
 		return nil, err
 	}
+	logrus.WithFields(logrus.Fields{
+		"function": "FindMembersByCommunityId",
+	}).Info(members)
 	return members, nil
 }
 
@@ -73,9 +109,17 @@ func (*CommunityDaoImpl) FindPlayersByCommunityId(
 	communityId community.CommunityId,
 ) ([]entity.Player, error) {
 	var players []entity.Player
-	if err := db.Where("community_id = ?", communityId.Value()).Find(&players).Error; err != nil {
+	if err := db.
+		Where("community_id = ?", communityId.Value()).
+		Find(&players).Error; err != nil {
+		logrus.WithFields(logrus.Fields{
+			"function": "FindPlayersByCommunityId",
+		}).Error(err)
 		return nil, err
 	}
+	logrus.WithFields(logrus.Fields{
+		"function": "FindPlayersByCommunityId",
+	}).Info(players)
 	return players, nil
 }
 
@@ -92,7 +136,13 @@ func (*CommunityDaoImpl) FindPlayersWithStatusByCommunityId(
 			status.Value(),
 		).
 		Find(&players).Error; err != nil {
+		logrus.WithFields(logrus.Fields{
+			"function": "FindPlayersWithStatusByCommunityId",
+		}).Error(err)
 		return nil, err
 	}
+	logrus.WithFields(logrus.Fields{
+		"function": "FindPlayersWithStatusByCommunityId",
+	}).Info(players)
 	return players, nil
 }
